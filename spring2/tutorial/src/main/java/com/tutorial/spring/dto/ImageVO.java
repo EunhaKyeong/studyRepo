8932da8a029b3683777a8c13de1c4e5d56f0af2b
EunhaKyeong <eh0634a@gmail.com>
2020-12-21 21:52:11 +0900

    image 테이블 VO 생성

---
+++ spring2/tutorial/src/main/java/com/tutorial/spring/dto/ImageVO.java
@@ -0,0 +1,28 @@
+package com.tutorial.spring.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImageVO {
+	private int imageId;
+	private String imageName;
+	private String imageUrl;
+	
+	public int getImageId() {
+		return imageId;
+	}
+	public void setImageId(int imageId) {
+		this.imageId = imageId;
+	}
+	public String getImageName() {
+		return imageName;
+	}
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+}
