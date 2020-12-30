a6f7a0e4f301ddae72a165afca8fbdaaea05e557
EunhaKyeong <eh0634a@gmail.com>
2020-12-30 12:41:01 +0900

    ajax 구현 javascript 파일

--- JSP_Ajax/ajax/src/main/webapp/resources/js/index.js
+++ JSP_Ajax/ajax/src/main/webapp/resources/js/index.js
@@ -1,28 +1,33 @@
 $("#registerButton").click(function() {
 	var name = $("#registerName").val();	//user 이름
-	var age = $("#registerAge").val();	//user 나이
+	var age = Number($("#registerAge").val());	//user 나이
 	var gender = $("input:radio[name='registerGender']:checked").val();	//user 성별
 	var email = $("#registerEmail").val();	//user 이메일
-	
+	var form = {"userName" : name,
+				"userAge" : age, 
+				"userGender" : gender, 
+				"userEmail" : email};
+			
 	$.ajax({
-		url : "/insertUser",
-		type: "POST",
-		data : {"name" : name,
-				"age" : age, 
-				"gender" : gender, 
-				"email" : email}, 
+		url: "/",
+	    type: "POST",
+	    data: JSON.stringify(form),
+	    contentType: "application/json; charset=utf-8;",
+	    dataType: "json",
 		success : function(data) {
 			var html = "";
-			html += "<td>" + data.name + "</td>";
-			html += "<td>" + data.age + "</td>";
-			html += "<td>" + data.gender + "</td>";
-			html += "<td>" + data.email + "</td>";
-			
-			$("#ajaxTr").html(html);
+			html += "<tr>";
+			html += "<td>" + data.userName + "</td>";
+			html += "<td>" + data.userAge + "</td>";
+			html += "<td>" + data.userGender + "</td>";
+			html += "<td>" + data.userEmail + "</td>";
+			html += "</tr>";
+					
+			$("#ajaxTable").append(html);
 			alert("등록 성공");
 		},
 		error : function() {
 			alert("등록 실패");
 		}
 	});
-})
+});
