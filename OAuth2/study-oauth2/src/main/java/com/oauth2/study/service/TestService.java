db8073b666c88a3a2862493eb8732031b0e509a3
EunhaKyeong <eh0634a@gmail.com>
2021-01-14 22:57:55 +0900

    test 스키마 쿼리문과 관련된 서비스 패키지 추가

---
+++ OAuth2/study-oauth2/src/main/java/com/oauth2/study/service/TestService.java
@@ -0,0 +1,10 @@
+package com.oauth2.study.service;
+
+import java.util.List;
+
+import com.oauth2.study.dto.UserVO;
+
+public interface TestService {
+	public List<UserVO> getAllUser();
+	public int checkIdNaver(String userId);
+}
---
+++ OAuth2/study-oauth2/src/main/java/com/oauth2/study/service/TestServiceImpl.java
@@ -0,0 +1,30 @@
+package com.oauth2.study.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.oauth2.study.dto.UserVO;
+import com.oauth2.study.mapper.TestMapper;
+
+@Service
+public class TestServiceImpl implements TestService {
+	@Inject
+	TestMapper testMapper;
+	
+	@Override
+	public List<UserVO> getAllUser() {
+		List<UserVO> userVOList = testMapper.selectAlluser();
+		
+		return userVOList;
+	}
+
+	@Override
+	public int checkIdNaver(String userId) {
+		int checkuserId = testMapper.checkUserIdNaver(userId);
+		
+		return checkuserId;
+	}
+}
