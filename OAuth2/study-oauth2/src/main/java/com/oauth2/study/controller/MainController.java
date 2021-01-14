822c57aa1a9083463367fb771605cc00cf4da185
EunhaKyeong <eh0634a@gmail.com>
2021-01-14 22:58:47 +0900

    210114 네이버 소셜로그인 공부중

--- OAuth2/study-oauth2/src/main/java/com/oauth2/study/controller/MainController.java
+++ OAuth2/study-oauth2/src/main/java/com/oauth2/study/controller/MainController.java
@@ -1,10 +1,17 @@
 package com.oauth2.study.controller;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.oauth2.study.dto.UserVO;
+import com.oauth2.study.service.TestServiceImpl;
+import java.util.List;
+
 @Controller
 public class MainController {
+	
 	@RequestMapping(value="/")
 	public String index() {
 		
