64fe76bfa066ac3c833dd5438ac29616b7e5ba2c
EunhaKyeong <eh0634a@gmail.com>
2021-01-17 20:02:49 +0900

    주석 좀 더 자세하게 추가!

--- OAuth2/study-oauth2/src/main/java/com/oauth2/study/auth/SnsValue.java
+++ OAuth2/study-oauth2/src/main/java/com/oauth2/study/auth/SnsValue.java
@@ -1,10 +1,13 @@
-//각 sns의 종류, clientId, clientSecret, redirectUrl(=callback)의 정보를 담는 일종의 VO 역할
 package com.oauth2.study.auth;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
 
 import lombok.Data;
 
+/*
+ * 각 sns의 종류, clientId, clientSecret, redirectUrl(=callback)의 정보를 담는 일종의 VO 역할
+ * sns의 정보는 이곳에 다 담는다고 생각하면 됨.
+ */
 @Data
 public class SnsValue implements SnsUrls{
 	private String service;
@@ -19,8 +22,10 @@ public class SnsValue implements SnsUrls{
 		 this.clientId = clientId;
 		 this.clientSecret = clientSecret; 
 		 this.redirectUrl = redirectUrl; 
+		 //servlet-context에서 service, clientId, clientSecret, redirectUrl은 빈등록할 때 함께 저장하지만 
+		 //profileUrl, 구현체는 함께 저장해주지 않아서 서비스별로 저장해줘야 함.
 		 if (service.equals("naver")) {
-			 this.api20Instance = NaverAPI20.instance();
+			 this.api20Instance = NaverAPI20.instance();	//서비스별 구현체를 지정.
 			 this.profileUrl = NAVER_PROFILE;
 		 }
 	}
